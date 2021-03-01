fb589f5b99bf840e2dba73cf94dea6e6c8238a60
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-03-01 12:53:59 +0200

    Add id and list to ScreenConfig

--- lib/screen_config.dart
+++ lib/screen_config.dart
@@ -1,11 +1,26 @@
 import 'package:equatable/equatable.dart';
 
 class ScreenConfig extends Equatable {
+  static const list = [
+    ScreenConfig(idLeucovorin, 'Leukovarino skaičiuoklė', '/vial-calculation'),
+    ScreenConfig(idVial, 'Flakonų skaičiuoklė', '/leucovorin-calculation')
+  ];
+  static const idLeucovorin = 'LEUCOVORIN';
+  static const idVial = 'VIAL';
+
+  final String id;
   final String title;
   final String route;
 
-  const ScreenConfig(this.title, this.route);
+  const ScreenConfig(this.id, this.title, this.route);
+
+  static String getRouteById(String id) => list.getById(id).route;
 
   @override
-  List<Object> get props => [title, route];
+  List<Object> get props => [id, title, route];
+}
+
+extension ScreenConfigSearch on List<ScreenConfig> {
+  ScreenConfig getById(String id) =>
+      ScreenConfig.list.firstWhere((element) => element.id == id);
 }
