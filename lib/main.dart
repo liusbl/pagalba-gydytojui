f982cbeb19846f2923bc7063ea43c9b41a72d269
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-03-01 12:57:27 +0200

    Add very basic navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pagalba_gydytojui/main_screen.dart';
+import 'package:pagalba_gydytojui/screen_config.dart';
 
 void main() {
   runApp(App());
@@ -9,6 +10,10 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        ScreenConfig.getRouteById(ScreenConfig.idLeucovorin): (context) => Text('asd'),
+        ScreenConfig.getRouteById(ScreenConfig.idVial): (context) => Text('dsa'),
+      },
       title: 'Pagalba Gydytojui',
       theme: ThemeData(
         primarySwatch: Colors.blue,
