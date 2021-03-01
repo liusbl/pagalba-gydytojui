d8943393f76bee5204ec9f4213e36728f24a1695
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2021-03-01 10:26:40 +0200

    Configure Equatable in screen_config

--- lib/screen_config.dart
+++ lib/screen_config.dart
@@ -1,6 +1,11 @@
-class ScreenConfig {
+import 'package:equatable/equatable.dart';
+
+class ScreenConfig extends Equatable {
   final String title;
   final String route;
 
   const ScreenConfig(this.title, this.route);
+
+  @override
+  List<Object> get props => [title, route];
 }
